99b5f7c9da86f6562ceaeedb00d524204ac0b678
Antoney Nyaga <antoneynyaga01@gmail.com>
2023-04-30 10:05:23 +0300

    Changes

--- cpanel/delete_org.php
+++ cpanel/delete_org.php
@@ -1,6 +1,17 @@
+<?php
 
+//Include authentication
+require("process/auth.php");
 
+//Include database connection
+require("../config/db.php");
 
+//Include class Organization
+require("classes/Organization.php");
+
+?>
+
+<?php include_once ("inc/header.php");?>
 
 <div class="container">
         <div class="row">
