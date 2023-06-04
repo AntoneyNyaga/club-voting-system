9181f4d5f92d7c6aeedefaf9ea0c052f1c62ef5b
Antoney Nyaga <antoneynyaga01@gmail.com>
2023-06-04 17:06:14 +0300

    edit

--- cpanel/activate.php
+++ cpanel/activate.php
@@ -1,13 +1,4 @@
-<?php
 
-//Include authentication
-require("process/auth.php");
-
-//Include database connection
-require("../config/db.php");
-
-//Include class Organization
-require("classes/Organization.php");
 
 ?>
 <?php include_once("inc/header.php"); ?><?php
