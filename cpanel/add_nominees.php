6b76753f9f0a39459c926c7895f993dc9f32f2ef
Antoney Nyaga <antoneynyaga01@gmail.com>
2023-06-05 17:08:55 +0300

    edit

--- cpanel/add_nominees.php
+++ cpanel/add_nominees.php
@@ -1,20 +1,5 @@
 <?php
 
-//Include authentication
-require("process/auth.php");
-
-//Include database connection
-require("../config/db.php");
-
-//Include class Organization
-require("classes/Organization.php");
-
-//Include class Position
-require("classes/Position.php");
-
-//Include class Nominees
-require("classes/Nominees.php");
-
 ?>
 <?php include_once("inc/header.php"); ?>
 
