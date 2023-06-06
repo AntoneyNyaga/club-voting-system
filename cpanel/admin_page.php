8d9ddef6c3eafc7b4c193971f57752a7f9f723fb
Antoney Nyaga <antoneynyaga01@gmail.com>
2023-06-06 17:10:15 +0300

    edit

--- cpanel/admin_page.php
+++ cpanel/admin_page.php
@@ -1,17 +1,4 @@
 
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-12 jumbotron">
-            <div class="text-center">
-                <h2>Internal Student Voting System</h2>
-                <p>
-                    <h1>Voting System</h1><br />
-                <button class="btn btn-info">Welcome</button>
-                </p>
-            </div>
-        </div>
-    </div>
-</div>
 
 <?php include_once ("inc/footer.php");?>
