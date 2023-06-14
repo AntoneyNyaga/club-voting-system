b7d69390e3211175d1bac0ca18d6232a7d63fe05
Antoney Nyaga <antoneynyaga01@gmail.com>
2023-06-14 10:00:26 +0300

    edit

--- cpanel/edit_pos.php
+++ cpanel/edit_pos.php
@@ -1,20 +1,6 @@
-<?php
-
-//Include authentication
-require("process/auth.php");
-
-//Include database connection
-require("../config/db.php");
-
-//Include class Organization
-require("classes/Organization.php");
-
-//Include class Position
-require("classes/Position.php");
-
-?>
 
 
+<?php include_once ("inc/header.php");?>
 
     <div class="container">
         <div class="row">
