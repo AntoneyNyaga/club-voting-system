89c93af7d0378d98fae1318a28913a73ff8ae56c
Antoney Nyaga <antoneynyaga01@gmail.com>
2023-06-14 10:00:48 +0300

    Edit

--- cpanel/edit_pos.php
+++ cpanel/edit_pos.php
@@ -1,4 +1,18 @@
+<?php
 
+//Include authentication
+require("process/auth.php");
+
+//Include database connection
+require("../config/db.php");
+
+//Include class Organization
+require("classes/Organization.php");
+
+//Include class Position
+require("classes/Position.php");
+
+?>
 
 <?php include_once ("inc/header.php");?>
 
