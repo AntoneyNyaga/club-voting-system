e65860153ad33905c879928c6609e1e99aa6f681
Antoney Nyaga <antoneynyaga01@gmail.com>
2023-06-15 00:29:19 +0300

    Edit

--- cpanel/delete_org.php
+++ cpanel/delete_org.php
@@ -16,16 +16,7 @@ require("classes/Organization.php");
 <div class="container">
         <div class="row">
             <div class="col-md-4 col-md-offset-4">
-                <?php
-                if(isset($_GET['id'])) {
-                    $org_id = trim($_GET['id']);
-
-                    echo "<div class='alert alert-danger'>Are you sure you want to delete selected club? <a href='http://localhost/voting/cpanel/delete_org.php?del_id=$org_id'>Yes</a> | <a href='http://localhost/voting/cpanel/add_org.php'>No</a></div>";
-                }
-
-
-                if(isset($_GET['del_id'])) {
-                    $id_to_del = $_GET['del_id'];
+          
 
                     $delOrg = new Organization();
                     $rtnDelOrg = $delOrg->DELETE_ORG($id_to_del);
