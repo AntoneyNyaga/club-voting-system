ec3c7551f2d9c5cf043843c8697ebed0b8bb1b98
Antoney Nyaga <antoneynyaga01@gmail.com>
2023-06-18 01:25:42 +0300

    Edit

--- cpanel/delete_nominee.php
+++ cpanel/delete_nominee.php
@@ -30,13 +30,6 @@ require("classes/Nominees.php");
             }
 
 
-            if(isset($_GET['del_id'])) {
-                $id_to_del = $_GET['del_id'];
-
-                $delNom = new Nominees();
-                $rtnDelNom = $delNom->DELETE_NOMINEE($id_to_del);
-            }
-            ?>
         </div>
     </div>
 </div>
