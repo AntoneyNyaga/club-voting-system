2e39f3000a676f8337b63c6089c21b99fd1201c2
Antoney Nyaga <antoneynyaga01@gmail.com>
2023-06-18 01:23:42 +0300

    Edit

--- cpanel/check.php
+++
@@ -1,7 +0,0 @@
-<?php
-  include "config.php";
-  $s=$_POST['active'];
-  $s=$_POST['rmvfile'];
-  $sel="update organization set status=$status where id='".$_POST['rmvfile']."'";  //your query
- $sel1=mysql_query($sel);
- ?>
