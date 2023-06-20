435a53ef8a1bb130b9b5e6899d2b654dae1d9ec3
Antoney Nyaga <antoneynyaga01@gmail.com>
2023-06-20 10:23:13 +0300

    Edit

--- cpanel/delete_voter.php
+++ cpanel/delete_voter.php
@@ -1,11 +1,6 @@
 <?php
 
-//Include authentication
-require("process/auth.php");
-
-//Include database connection
-require("../config/db.php");
-
+//Include class Voters
 require("classes/Voters.php");
 
 ?>
