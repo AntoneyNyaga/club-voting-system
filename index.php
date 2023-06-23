eddd7cb0ac038d9ac2cfa90d4e866d37eb949750
Antoney Nyaga <antoneynyaga01@gmail.com>
2023-06-24 00:58:04 +0300

    Mnor Edits

--- index.php
+++ index.php
@@ -14,8 +14,7 @@ unset($_SESSION['STUD_ID']);
 <div class="container">
     <div class="row">
         <div class="col-md-4 col-md-offset-4">
-            <div class="login-con">
-                <h3>Student Login</h3><hr>
+
                 <?php
                 if(isset($_SESSION['ERROR_MSG_ARRAY']) && is_array($_SESSION['ERROR_MSG_ARRAY']) && COUNT($_SESSION['ERROR_MSG_ARRAY']) > 0) {
                     foreach($_SESSION['ERROR_MSG_ARRAY'] as $msg) {
