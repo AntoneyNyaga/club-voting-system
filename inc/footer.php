63f9899dc20857179585209843770965e8657904
Antoney Nyaga <antoneynyaga01@gmail.com>
2023-06-28 14:54:59 +0300

    Edits

--- inc/footer.php
+++ inc/footer.php
@@ -1,15 +1,3 @@
-<!-- Footer -->
-<nav class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
-
-    <div class="container">
-        <div class="navbar-text pull-left">
-        Copyright <?php echo date("d-m-Y") ?>
-
-        </div>
-    </div>
-
-</nav>
-<!-- End Footer -->
 
 <script src="assets/js/jquery.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
