bc5fe94980fac50185d5cc44049df6155e9dca00
Antoney Nyaga <antoneynyaga01@gmail.com>
2023-07-01 15:03:58 +0300

    Edit

--- cpanel/add_nominees.php
+++ cpanel/add_nominees.php
@@ -6,8 +6,7 @@ require("process/auth.php");
 //Include database connection
 require("../config/db.php");
 
-//Include class Organization
-require("classes/Organization.php");
+
 
 //Include class Position
 require("classes/Position.php");
--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,12 @@
+<!-- Footer -->
+<nav class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+
+    <div class="container">
+        <div class="navbar-text pull-left">
+        Copyright <?php echo date("d-m-Y") ?>
+
+        </div>
+    </div>
 
 </nav>
 <!-- End Footer -->
