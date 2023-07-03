2995eb6c6e717fc07a5263178392805e95febd7d
Antoney Nyaga <you@example.com>
2023-07-03 09:30:58 +0300

    Edit

--- cpanel/delete_pos.php
+++ cpanel/delete_pos.php
@@ -1,15 +1,4 @@
-<?php
 
-//Include authentication
-require("process/auth.php");
-
-//Include database connection
-require("../config/db.php");
-
-//Include class Position
-require("classes/Position.php");
-
-?>
 
 <?php include_once ("inc/header.php");?>
 
