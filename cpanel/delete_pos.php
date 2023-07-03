409446286510999e67620c3c90a3e2bfc6eaa694
Antoney Nyaga <you@example.com>
2023-07-03 09:31:26 +0300

    Edit minor

--- cpanel/delete_pos.php
+++ cpanel/delete_pos.php
@@ -1,4 +1,15 @@
+<?php
 
+//Include authentication
+require("process/auth.php");
+
+//Include database connection
+require("../config/db.php");
+
+//Include class Position
+require("classes/Position.php");
+
+?>
 
 <?php include_once ("inc/header.php");?>
 
